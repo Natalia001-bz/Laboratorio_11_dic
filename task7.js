067a34e195a3d0c407ae3deca45b5de0e82efc6d
Natalia001-bz <mesa.cc1257@gmail.com>
2024-12-11 11:15:10 -0500

    lab

--- task7.js
+++ task7.js
@@ -2,12 +2,3 @@ export function rubricPerfect() {
 
 }
 
-export function costCalculator(cost) {
-    const fee=  3
-    const interestFee =  0.01
-    let monthlyPayments = parseFloat(cost)
-    let totalPayments = monthlyPayments+fee+(monthlyPayments*interestFee)
-    //return `El valor total que debes pagar es: ${cant + interes + tarifaFija}`
-    return totalPayments
-
-}
